18957feff790bbeab7c6256550b6878947211784
Andrés Rueda <104632202+AndrewS124@users.noreply.github.com>
2024-05-26 16:57:20 +0000

    Actualizar .gitignore

--- Easy/problema6.java
+++ Easy/problema6.java
@@ -2,13 +2,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class problema6 {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //cont fichas
+        // cont fichas
         String entrada = br.readLine();
 
         String[] tamMatriz = entrada.split(" ");
@@ -16,7 +15,7 @@ public class problema6 {
         int filas = Integer.parseInt(tamMatriz[0]);
         int columnas = Integer.parseInt(tamMatriz[1]);
 
-        int fichasIngresada = (filas*columnas)/2;
+        int fichasIngresada = (filas * columnas) / 2;
 
         System.out.println(fichasIngresada);
 
